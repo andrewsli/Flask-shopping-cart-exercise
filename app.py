6342a7fa836e5c6de89fdd6a42a3dc6005f17eef
Andrew Li <andrewli@Christinas-MBP.localdomain>
2019-06-20 16:56:18 -0700

    mostly working version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Shopping cart application."""
 
-from flask import Flask, request, session, render_template
+from flask import Flask, request, session, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from products import Product
@@ -14,23 +14,45 @@ debug = DebugToolbarExtension(app)
 @app.route("/")
 def homepage():
     """Homepage: show list of products with link to product page."""
+    if not session:
+        session["cart"] = []
 
-    return render_template("homepage.html")
+    all_products = Product.get_all()
+    return render_template("homepage.html", products=all_products)
 
 @app.route("/products/<int:product_id>")
 def product_detail(product_id):
     """Show detail of product, along with add-to-cart form."""
+    product_object = Product.get_by_id(product_id)
+    return render_template("product.html", product=product_object)
 
-    return render_template("product.html")
 
 @app.route("/cart")
 def show_cart():
-    """Show shopping cart."""
+    """List items in shopping cart, prices, and total"""
+    total_price = 0
+    cart_list = [Product.get_by_id(id) for id in session['cart']]
 
-    return render_template("cart.html")
+    for product in cart_list:
+        total_price += product.price
+
+    return render_template("cart.html", total=total_price, products=cart_list)
 
 
+@app.route("/add-to-cart", methods=["POST"])
+def add_to_cart():
+    """Add product ID to session cart, redirect user to homepage with flashed
+        confirmation message
+    """
+
+    product_id = request.form["product_id"]
+    product_name = request.form["product_name"]
+
+    session["cart"].append(product_id)
+    session.modified = True
+
+    flash(f"{product_name} has been added to your cart.")
+    return redirect("/")
 
 # missing routes:
-#   /add-to-cart
 #   /clear-cart   [in further study]
